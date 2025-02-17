66e33380697b9b34f3ae29f9e571d29f4b336f3a
Perry Robotics <phsrambots@gmail.com>
2025-02-17 11:27:11 -0500

    2025-Reefscape-Code

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -68,7 +68,7 @@ public class TunerConstants {
     // CAN bus that the devices are located on;
     // All swerve devices must share the same CAN bus
     public static final CANBus kCANBus = new CANBus("rio", "./logs/example.hoot");
-    public static final CANBus kCANBus2 = new CANBus("drivetrain", "./logs/example.hoot");
+    public static final CANBus kCANBus2 = new CANBus("drivetrain", "./logs/example.hoot"); //TODO Canivore
 
     // Theoretical free speed (m/s) at 12 V applied output;
     // This needs to be tuned to your individual robot
